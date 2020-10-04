009e5f0995e56d6beed4dbdac27f89da901e9ab3
Alexander Bernauer <acopton@gmail.com>
2020-10-04 13:06:54 +0200

    enumerating all cards

--- src/Cards.hs
+++ src/Cards.hs
@@ -35,3 +35,33 @@ data Card = Card
     { cardKind :: Kind
     , cardSuit :: Suit
     } deriving (Show, Eq)
+
+-- | Enumerate all cards, first by kind then by suit
+--
+-- Examples:
+-- >>> length ([minBound .. maxBound] :: [Card])
+-- 52
+--
+-- >>> minBound :: Card
+-- Card {cardKind = Two, cardSuit = Hearts}
+--
+-- >>> toEnum 12 :: Card
+-- Card {cardKind = Ace, cardSuit = Hearts}
+--
+-- >>> maxBound :: Card
+-- Card {cardKind = Ace, cardSuit = Clubs}
+
+instance Bounded Card where
+    minBound = Card minBound minBound
+    maxBound = Card maxBound maxBound
+
+numberofKinds :: Int
+numberofKinds = fromEnum (maxBound :: Kind) + 1
+
+instance Enum Card where
+    toEnum n =
+        let (d, m) = divMod n numberofKinds
+        in  Card (toEnum m) (toEnum d)
+
+    fromEnum (Card k s) =
+        (fromEnum s) * numberofKinds + (fromEnum k)
--- test/Spec.hs
+++ test/Spec.hs
@@ -3,10 +3,13 @@ import Test.Tasty
 import qualified TestCombinations as TestCombinations
 import qualified TestHands as TestHands
 import qualified QuickPropHands as QuickPropHands
+import Test.DocTest (doctest)
 
 main :: IO ()
-main = defaultMain $ testGroup "holdem tests"
-    [ TestCombinations.tests
-    , TestHands.tests
-    , QuickPropHands.tests
-    ]
+main = do
+    doctest ["-isrc", "src/Cards.hs"]
+    defaultMain $ testGroup "holdem tests"
+        [ TestCombinations.tests
+        , TestHands.tests
+        , QuickPropHands.tests
+        ]
